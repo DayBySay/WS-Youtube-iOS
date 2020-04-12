5ba49054040d8068883fe7fbc0cca81db223fc6b
daybysay <yukita.sekai+github@gmail.com>
2020-04-12 22:59:30 +0900

    add ios project

---
+++ WS-Youtube/WS-Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WS-Youtube
+//
+//  Created by Takayuki Sei on 2020/04/12.
+//  Copyright © 2020 takayuki sei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
