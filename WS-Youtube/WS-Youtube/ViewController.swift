bde973799c994343c02fe9e67166791a2ae74cf1
daybysay <yukita.sekai+github@gmail.com>
2020-04-12 23:27:43 +0900

    play youtube video

--- WS-Youtube/WS-Youtube/ViewController.swift
+++ WS-Youtube/WS-Youtube/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import YoutubePlayerView
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var youtubeView: YoutubePlayerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let playerVars: [String: Any] = [
+            "controls": 1,
+            "modestbranding": 1,
+            "playsinline": 1,
+            "rel": 0,
+            "showinfo": 0,
+            "autoplay": 1
+        ]
+        youtubeView.loadWithVideoId("1JdMxfTjkPE", with: playerVars)
     }
-
-
 }
 
